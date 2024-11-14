bb0292c66a3e4a7e9d1d39b00ceb93270767ee02
milliax <maxchengboy@gmail.com>
2024-11-14 13:41:09 +0800

    update the concept

---
+++ hw_MCRAFT/README.md
@@ -0,0 +1,4 @@
+# this is the homework of the MCRAFT practice
+
+using the python code to arrange the facility
+according to the flow chart.
---
+++ hw_MCRAFT/fixed_band.py
@@ -0,0 +1,25 @@
+""" Define the layout chart """
+WIDTH = 5
+HEIGHT = 3
+BAND_HEIGHT = 3
+
+AREA_REQUIRED = {
+    "A": 10,
+    "B": 5,
+}
+
+""" Define the layout chart """
+TAG_NAMES = ["A","B"]
+FROM_TO_CHART = [
+    [0,1],
+    [1,0],
+]
+
+""" Find the middle point of the part """
+
+
+
+""" Examination Part """
+
+
+""" MAIN Program """
